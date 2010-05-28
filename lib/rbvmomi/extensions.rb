13c057860e3d8f8cf1fd06714b81d286e2350d7d
Rich Lane <rlane@vmware.com>
2010-05-28 13:24:33 -0700

    Task#wait_for_progress

--- lib/rbvmomi/extensions.rb
+++ lib/rbvmomi/extensions.rb
@@ -39,6 +39,19 @@ class Task
       raise info.error
     end
   end
+
+  def wait_for_progress
+    wait_until('info.state', 'info.progress') do
+      yield info.progress if block_given?
+      %w(success error).member? info.state
+    end
+    case info.state
+    when 'success'
+      info.result
+    when 'error'
+      raise info.error
+    end
+  end
 end
 
 Folder
