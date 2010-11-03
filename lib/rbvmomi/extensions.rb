6658a65b15e585ac777b618bba7e1d086b616302
Rich Lane <rlane@vmware.com>
2010-11-03 10:23:49 -0700

    compute resource stats

--- lib/rbvmomi/extensions.rb
+++ lib/rbvmomi/extensions.rb
@@ -445,4 +445,49 @@ class OvfManager
   end
 end
 
+ComputeResource
+class ComputeResource
+  def stats
+    filterSpec = VIM.PropertyFilterSpec(
+      objectSet: [{
+        obj: self,
+        selectSet: [
+          VIM.TraversalSpec(
+            name: 'tsHosts',
+            type: 'ComputeResource',
+            path: 'host',
+            skip: false,
+          )
+        ]
+      }],
+      propSet: [{
+        pathSet: %w(name overallStatus summary.hardware.cpuMhz
+                    summary.hardware.numCpuCores summary.hardware.memorySize
+                    summary.quickStats.overallCpuUsage
+                    summary.quickStats.overallMemoryUsage),
+        type: 'HostSystem'
+      }]
+    )
+
+    result = @soap.propertyCollector.RetrieveProperties(specSet: [filterSpec])
+
+    stats = {
+      totalCPU: 0,
+      totalMem: 0,
+      usedCPU: 0,
+      usedMem: 0,
+    }
+
+    result.each do |x|
+      next if x['overallStatus'] == 'red'
+      stats[:totalCPU] += x['summary.hardware.cpuMhz'] * x['summary.hardware.numCpuCores']
+      stats[:totalMem] += x['summary.hardware.memorySize'] / (1024*1024)
+      stats[:usedCPU] += x['summary.quickStats.overallCpuUsage']
+      stats[:usedMem] += x['summary.quickStats.overallMemoryUsage']
+    end
+
+    stats
+  end
+end
+
 end
