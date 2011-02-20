15cbae3361729fb85ddddc9e31f8beb0fa3c8288
Rich Lane <rlane@club.cc.cmu.edu>
2011-02-20 14:07:41 -0800

    document ResourcePool extensions

--- lib/rbvmomi/vim/ResourcePool.rb
+++ lib/rbvmomi/vim/ResourcePool.rb
@@ -1,9 +1,18 @@
 class RbVmomi::VIM::ResourcePool
+  # Retrieve a child ResourcePool.
+  # @param name [String] Name of the child.
+  # @return [VIM::ResourcePool]
+  def find name
+    @soap.searchIndex.FindChild(entity: self, name: name)
+  end
+
+  # Retrieve a descendant of this ResourcePool.
+  # @param path [String] Path delimited by '/'.
+  # @return [VIM::ResourcePool]
   def traverse path
     es = path.split('/').reject(&:empty?)
-    return self if es.empty?
     es.inject(self) do |f,e|
-      @soap.searchIndex.FindChild(entity: f, name: e) || return
+      f.find(e) || return
     end
   end
 end
