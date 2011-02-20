86bbc078b5c16846ef4d643580fbb846ce70f188
Rich Lane <rlane@club.cc.cmu.edu>
2011-02-20 12:40:43 -0800

    document ManagedEntity extensions

--- lib/rbvmomi/vim/ManagedEntity.rb
+++ lib/rbvmomi/vim/ManagedEntity.rb
@@ -1,4 +1,6 @@
 class RbVmomi::VIM::ManagedEntity
+  # Retrieve the ancestors of the entity.
+  # @return [Array] Ancestors of this entity, starting with the root.
   def path
     filterSpec = RbVmomi::VIM.PropertyFilterSpec(
       objectSet: [{
@@ -35,6 +37,9 @@ class RbVmomi::VIM::ManagedEntity
     a.reverse
   end
 
+  # Return a string representation of +path+ suitable for display.
+  # @return [String]
+  # @see #path
   def pretty_path
     path[1..-1].map { |x| x[1] } * '/'
   end
