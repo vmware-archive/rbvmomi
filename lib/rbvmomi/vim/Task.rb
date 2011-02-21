73f02816f37f84b2262fffe3ec59657493048e06
Rich Lane <rlane@club.cc.cmu.edu>
2011-02-20 16:00:37 -0800

    document Task extensions

--- lib/rbvmomi/vim/Task.rb
+++ lib/rbvmomi/vim/Task.rb
@@ -1,4 +1,7 @@
 class RbVmomi::VIM::Task
+  # Wait for a task to finish.
+  # @return +info.result+ on success.
+  # @raise +info.error+ on error.
   def wait_for_completion
     wait_until('info.state') { %w(success error).member? info.state }
     case info.state
@@ -9,6 +12,10 @@ class RbVmomi::VIM::Task
     end
   end
 
+  # Wait for a task to finish, with progress notifications.
+  # @return (see #wait_for_completion)
+  # @raise (see #wait_for_completion)
+  # @yield [info.progress]
   def wait_for_progress
     wait_until('info.state', 'info.progress') do
       yield info.progress if block_given?
