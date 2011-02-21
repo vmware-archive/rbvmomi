5fbaae1ba643ba98f3fe44cfa96455042b341680
Rich Lane <rlane@club.cc.cmu.edu>
2011-02-20 16:02:42 -0800

    document VirtualMachine extensions

--- lib/rbvmomi/vim/VirtualMachine.rb
+++ lib/rbvmomi/vim/VirtualMachine.rb
@@ -1,4 +1,6 @@
 class RbVmomi::VIM::VirtualMachine
+  # Retrieve the MAC addresses for all virtual NICs.
+  # @return [Hash] Keyed by device label.
   def macs
     Hash[self.config.hardware.device.grep(RbVmomi::VIM::VirtualEthernetCard).map { |x| [x.deviceInfo.label, x.macAddress] }]
   end
