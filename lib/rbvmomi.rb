f00660d2a483eeb03604348d629430e764b7a4eb
Rich Lane <rlane@club.cc.cmu.edu>
2011-02-21 12:30:00 -0800

    add deprecated alias for VIM.connect

--- lib/rbvmomi.rb
+++ lib/rbvmomi.rb
@@ -1,4 +1,12 @@
 # Copyright (c) 2010 VMware, Inc.  All Rights Reserved.
-module RbVmomi; end
+module RbVmomi
+
+# @private
+# @deprecated Use +RbVmomi::VIM.connect+.
+def self.connect opts
+  VIM.connect opts
+end
+
+end
 require 'rbvmomi/connection'
 require 'rbvmomi/vim'
