24afcc96b2c19d99f4e2f3f2277039fe3895be6b
Rich Lane <rlane@club.cc.cmu.edu>
2011-11-05 15:25:38 -0700

    improve benchmark script

--- devel/benchmark.rb
+++ devel/benchmark.rb
@@ -15,8 +15,26 @@ def serialize obj, type, array=false
   $conn.obj2xml(xml, 'root', type, array, obj, attrs).target!
 end
 
-def deserialize str, type
-  $conn.xml2obj Nokogiri(str).root, type
+class OldDeserializer
+  def initialize conn
+    @conn = conn
+  end
+
+  def deserialize node
+    @conn.xml2obj node, nil
+  end
+end
+
+def diff a, b
+  a_io = Tempfile.new 'rbvmomi-diff-a'
+  b_io = Tempfile.new 'rbvmomi-diff-b'
+  PP.pp a, a_io
+  PP.pp b, b_io
+  a_io.close
+  b_io.close
+  system("diff -u #{a_io.path} #{b_io.path}")
+  a_io.unlink
+  b_io.unlink
 end
 
 dvport = VIM::DistributedVirtualPort(
@@ -213,8 +231,43 @@ do_serialize = lambda { serialize dvport, 'DistributedVirtualPort' }
 serialized_dvport = do_serialize[]
 parsed_dvport_nokogiri = Nokogiri(serialized_dvport)
 parsed_dvport_libxml = LibXML::XML::Parser.string(serialized_dvport).parse
-do_deserialize = lambda { deserialize serialized_dvport, 'DistributedVirtualPort' }
-deserialized_dvport = do_deserialize[]
+
+
+if true
+  nokogiri_xml = Nokogiri::XML(serialized_dvport).root
+  libxml_xml = LibXML::XML::Parser.string(serialized_dvport).parse.root
+
+  old_nokogiri_result = OldDeserializer.new($conn).deserialize nokogiri_xml
+  new_nokogiri_result = RbVmomi::Deserializer.new($conn).deserialize nokogiri_xml
+  new_libxml_result = RbVmomi::Deserializer.new($conn).deserialize libxml_xml
+
+  if new_nokogiri_result != old_nokogiri_result
+    puts "new_nokogiri_result doesnt match old_nokogiri_result"
+    puts "old_nokogiri_result:"
+    pp old_nokogiri_result
+    puts "new_nokogiri_result:"
+    pp new_nokogiri_result
+    puts "diff:"
+    diff old_nokogiri_result, new_nokogiri_result
+    exit 1
+  end
+
+  if new_libxml_result != old_nokogiri_result
+    puts "new_libxml_result doesnt match old_nokogiri_result"
+    puts "old_nokogiri_result:"
+    pp old_nokogiri_result
+    puts "new_libxml_result:"
+    pp new_libxml_result
+    puts "diff:"
+    diff old_nokogiri_result, new_libxml_result
+    exit 1
+  end
+
+  puts "all results match"
+end
+
+
+N = 1000
 
 def traverse node
   node.children.each do |child|
@@ -222,29 +275,24 @@ def traverse node
   end
 end
 
-N = 1000
-
 Benchmark.bmbm do|b|
 =begin
+  GC.start
   b.report("serialization") do
     N.times { do_serialize[] }
   end
-=end
 
   GC.start
-  
   b.report("nokogiri parsing") do
     N.times { Nokogiri(serialized_dvport) }
   end
   
   GC.start
-  
   b.report("recursive traversal of nokogiri") do
     N.times { traverse parsed_dvport_nokogiri.root }
   end
   
   GC.start
-
   b.report("libxml parsing") do
     N.times do
       LibXML::XML::Parser.string(serialized_dvport).parse
@@ -252,19 +300,20 @@ Benchmark.bmbm do|b|
   end
   
   GC.start
-  
   b.report("recursive traversal of libxml") do
     N.times { traverse parsed_dvport_libxml.root }
   end
+=end
   
   GC.start
-
-  b.report("deserialization") do
-    N.times { do_deserialize[] }
+  b.report("old deserialization (nokogiri)") do
+    deserializer = OldDeserializer.new($conn)
+    N.times do
+      deserializer.deserialize Nokogiri::XML(serialized_dvport).root
+    end
   end
 
   GC.start
-
   b.report("new deserialization (nokogiri)") do
     deserializer = RbVmomi::Deserializer.new($conn)
     N.times do
