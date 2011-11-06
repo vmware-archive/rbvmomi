39e7447d62e7b7365db9dfc14a2aac6362e58749
Christian Dickmann <cdickmann@vmware.com>
2011-11-06 12:30:24 +0100

    implement fetching of resourcePool hierarchies with a single SOAP request

--- lib/rbvmomi/vim/ResourcePool.rb
+++ lib/rbvmomi/vim/ResourcePool.rb
@@ -15,4 +15,46 @@ class RbVmomi::VIM::ResourcePool
       f.find(e) || return
     end
   end
+
+  def resourcePoolSubTree fields = []
+    self.class.resourcePoolSubTree self, fields
+  end
+  
+  def self.resourcePoolSubTree objs, fields = []
+    fields = (fields + ['name', 'resourcePool']).uniq
+    i = 0
+    filterSpec = RbVmomi::VIM.PropertyFilterSpec(
+      :objectSet => objs.map do |obj|
+        i += 1
+        RbVmomi::VIM.ObjectSpec(
+          :obj => obj,
+          :selectSet => [
+            RbVmomi::VIM.TraversalSpec(
+              :name => "tsME-#{i}",
+              :type => 'ResourcePool',
+              :path => 'resourcePool',
+              :skip => false,
+              :selectSet => [
+                RbVmomi::VIM.SelectionSpec(:name => "tsME-#{i}")
+              ]
+            )
+          ]
+        )
+      end,
+      :propSet => [{
+        :pathSet => fields,
+        :type => 'ResourcePool'
+      }]
+    )
+  
+    propCollector = objs.first._connection.propertyCollector
+    result = propCollector.RetrieveProperties(:specSet => [filterSpec])
+    
+    hash = Hash[result.map do |x| 
+      [
+        x.obj, 
+        Hash[ fields.map{|f| [f.to_sym, x.propSet.find{|y| y.name == f}.val] } ]
+      ]
+    end]
+  end
 end
