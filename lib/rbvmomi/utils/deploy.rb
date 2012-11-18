acb86089c1af2e4ee0ad63f87fac4a1c461cb5d0
Christian Dickmann <cdickmann@vmware.com>
2012-11-18 13:24:24 -0800

    fix

--- lib/rbvmomi/utils/deploy.rb
+++ lib/rbvmomi/utils/deploy.rb
@@ -181,7 +181,7 @@ class CachedOvfDeployer
         config = opts[:config] || {}
         config = vm.update_spec_add_delta_disk_layer_on_all_disks(config)
         # XXX: Should we add a version that does retries?
-        vm.ReconfigVM_Task(:spec => opts[:config]).wait_for_completion
+        vm.ReconfigVM_Task(:spec => config).wait_for_completion
         vm.MarkAsTemplate
       end
     end
