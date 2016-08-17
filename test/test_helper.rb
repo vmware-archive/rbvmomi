1dd677a7620be4aeb1b433b54beec57a2d72c3c6
J.R. Garcia <jr@garciaole.com>
2016-08-17 13:33:13 -0500

    Remove code coverage for 1.8
    SimpleCov only works with Ruby 1.9+. It seems that it doesn't just move
    on, but rather exits in failure. Removing code coverage for 1.8.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,7 @@
-require 'simplecov'
-SimpleCov.start { add_filter '/test/' }
+unless /^1.8/ =~ RUBY_VERSION
+  require 'simplecov'
+  SimpleCov.start { add_filter '/test/' }
+end
 
 require 'rbvmomi'
 VIM = RbVmomi::VIM
