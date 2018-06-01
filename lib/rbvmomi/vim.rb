1ffe04eeb41eed9acbf66ab5a08f699814ee24b7
Chao Li <chaol@vmware.com>
2018-06-01 11:01:21 +0800

    Revert "vim.rb: enhance the logic of vim.rev."
    This reverts commit 4efb0befa312304bd68a990b5749313aa2b577ec.

--- lib/rbvmomi/vim.rb
+++ lib/rbvmomi/vim.rb
@@ -39,7 +39,8 @@ class VIM < Connection
     opts[:port] ||= (opts[:ssl] ? 443 : 80)
     opts[:path] ||= '/sdk'
     opts[:ns] ||= 'urn:vim25'
-    opts[:rev] = '6.5' if opts[:rev].nil?
+    rev_given = opts[:rev] != nil
+    opts[:rev] = '6.5' unless rev_given
     opts[:debug] = (!ENV['RBVMOMI_DEBUG'].empty? rescue false) unless opts.member? :debug
 
     conn = new(opts).tap do |vim|
@@ -60,17 +61,10 @@ class VIM < Connection
             vim.serviceContent.sessionManager.Login :userName => opts[:user], :password => opts[:password]
         end
       end
-      # Version compare
-      vim.rev = [rev, opts[:rev]].min { |a, b|
-        a_parts = a.split('.')
-        b_parts = b.split('.')
-        result = 0
-        [a_parts.size, b_parts.size].min.times {|i|
-          result = (a_parts[i].to_i <=> b_parts[i].to_i)
-          break if result != 0
-        }
-        (result != 0) ? result : (a_parts.size <=> b_parts.size)
-      }
+      unless rev_given
+        rev = vim.serviceContent.about.apiVersion
+        vim.rev = [rev, '6.5'].min
+      end
     end
 
     at_exit { conn.close }
