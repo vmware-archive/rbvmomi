c09d30dc0388322faef820523e036a21485e27b7
J.R. Garcia <jrg@vmware.com>
2019-12-19 17:12:53 -0600

    Bump version to 2.3.0
    Bump version for release to fix bug.
    
    Signed-off-by: J.R. Garcia <jrg@vmware.com>

--- lib/rbvmomi/version.rb
+++ lib/rbvmomi/version.rb
@@ -1,6 +1,6 @@
-# Copyright (c) 2016-2017 VMware, Inc.  All Rights Reserved.
+# Copyright (c) 2016-2019 VMware, Inc.  All Rights Reserved.
 # SPDX-License-Identifier: MIT
 
 module RbVmomi
-  VERSION = '2.3.0.beta1'.freeze
+  VERSION = '2.3.0'.freeze
 end
