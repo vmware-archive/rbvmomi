96545d9d65a7ace03c60cb1b196a29f0d4f6410e
J.R. Garcia <jrg@vmware.com>
2020-07-31 17:42:39 -0500

    Bump version for release
    Upgrading major version due to breaking change (dropping support for EOL
    Ruby versions)
    
    Signed-off-by: J.R. Garcia <jrg@vmware.com>

--- lib/rbvmomi/version.rb
+++ lib/rbvmomi/version.rb
@@ -1,6 +1,6 @@
-# Copyright (c) 2016-2019 VMware, Inc.  All Rights Reserved.
+# Copyright (c) 2016-2020 VMware, Inc.  All Rights Reserved.
 # SPDX-License-Identifier: MIT
 
 module RbVmomi
-  VERSION = '2.4.1'.freeze
+  VERSION = '3.0.0'.freeze
 end
